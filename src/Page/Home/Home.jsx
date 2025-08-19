5b3bd9ae00523af5204757f3efbfea994dbdd732
abyssFrancesco <gragnaniellofrancescom@gmail.com>
2025-08-19 23:59:49 +0200

    dadadadad

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import './Home.css';
-import Navbar from '../../Components/NavBar/Navbar';
+import React from "react";
+import "./Home.css";
+import Navbar from "../../Components/NavBar/Navbar";
+import c from "../Home/../../assets/c.jpg";
 
 function Home() {
-
-    console.log("Home");
+  console.log("Home");
   return (
     <>
-    <div className="home B">
+      <div className="home-box B">
         <Navbar></Navbar>
-    </div>
+        <div className="home IB">
+          <div className="title IB">
+            <h1>Hello, Francesco here</h1>
+          </div>
+          <div className="img">
+            <img src={c} alt="" />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
